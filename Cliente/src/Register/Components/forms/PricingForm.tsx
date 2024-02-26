adc000eec6b06de8327abe8e51de6a8a0c3107a2
LennyMGarcia <lennymgr27@gmail.com>
2024-02-25 20:20:10 -0400

    test: Probando diferentes ideas dentro del pricing

--- Cliente/src/Register/Components/forms/PricingForm.tsx
+++ Cliente/src/Register/Components/forms/PricingForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, FormHelperText } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -9,6 +9,7 @@ import StarIcon from "@mui/icons-material/StarBorder";
 import Typography from "@mui/material/Typography";
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import { styled } from "@mui/system";
+import { ErrorMessage, Field, FieldProps} from "formik";
 
 
 const tiers = [
@@ -23,7 +24,7 @@ const tiers = [
   {
     title: "Familiar",
     subheader: "Mas popular",
-    position:"middle",
+    position: "middle",
     price: "1000",
     description: [
       "Status de procemientos en tiempo real",
@@ -45,82 +46,102 @@ const tiers = [
 ];
 
 const PricingList = styled("ul")({
-    margin: 0,
-    padding: 0,
-    listStyle: "none",
-  });
-  
+  margin: 0,
+  padding: 0,
+  listStyle: "none",
+});
+
 export default function PricingForm() {
   return (
-    <Container maxWidth="md" component="main">
-      <Grid container spacing={2} alignItems="flex-end">
-        {tiers.map((tier) => (
-          <Grid
-            item
-            key={tier.title}
-            xs={12}
-            sm={tier.title === "Enterprise" ? 12 : 6}
-            md={4}
-          >
-            <Card sx={{
-                  backgroundColor: tier.position == "middle" ? "#52b69a" : "white",
-                  boxShadow: tier.position == 'middle' ? "4px 4px" : 3,
-                  border: tier.position == 'middle' ? "2px black solid" : "none",
-                  borderRadius: "2vh",
-                  minHeight: tier.position == "middle" ? "60vh" : "50vh",
-                  width:"15rem",  display: 'flex', flexDirection: 'column', height: '100%' 
-                }}>
-              <CardHeader
-                title={tier.title}
-                subheader={tier.subheader}
-                titleTypographyProps={{ align: "center" }}
-                action={tier.title === "Pro" ? <StarIcon /> : null}
-                subheaderTypographyProps={{
-                  align: "center",
-                }}
-                
-              />
-              <CardContent>
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "baseline",
-                    mb: 2,
-                  }}
-                >
-                  <Typography component="h5" variant="h5" color="text.primary">
-                    RD${tier.price}
-                  </Typography>
-                  <Typography variant="h6" color="text.secondary">
-                    /me
-                  </Typography>
-                </Box>
-                <PricingList>
-                  {tier.description.map((line) => (
-                    <Typography
-                      component="li"
-                      variant="subtitle1"
-                      align="center"
-                      key={line}
-                    >
-                     <CheckCircleOutlineIcon sx={{color:"blue", fontSize:"1rem"}}/> {line}
-                    </Typography>
-                  ))}
-                </PricingList>
-              </CardContent>
-              <Box sx={{ flexGrow: 1 }} />
+    <Field
+      id="pricing"
+      name="pricing"
+    >
+      {({ form }: FieldProps) => (
+        //si van a usar este solo saquen el container
+        <Container maxWidth="md" component="main">
+          <Grid container spacing={2} alignItems="flex-end">
+            {tiers.map((tier, index) => (
+              <Grid
+                item
+                key={index}
+                xs={12}
+                sm={6}
+                md={4}
+              >
 
-  <CardActions  sx={{ width:"13rem", alignSelf: 'center' }}>
-                <Button fullWidth variant='contained'
-                 sx={{background: tier.position == 'middle'? "#168aad" : "#52b69a"}}>
-                  {tier.buttonText}
-                </Button>
-              </CardActions>
-            </Card>
+                <Box>
+                  <Card sx={{
+                    backgroundColor: tier.position === "middle" ? "#52b69a" : "white",
+                    boxShadow: tier.position === 'middle' ? "4px 4px" : 3,
+                    border: tier.position === 'middle' ? "2px black solid" : "none",
+                    borderRadius: "2vh",
+                    minHeight: tier.position === "middle" ? "60vh" : "50vh",
+                    width: "15rem", display: 'flex', flexDirection: 'column', height: '100%'
+                  }}>
+                    <CardHeader
+                      title={tier.title}
+                      subheader={tier.subheader}
+                      titleTypographyProps={{ align: "center" }}
+                      action={tier.title === "Pro" ? <StarIcon /> : null}
+                      subheaderTypographyProps={{
+                        align: "center",
+                      }}
+
+                    />
+                    <CardContent>
+                      <Box
+                        sx={{
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "baseline",
+                          mb: 2,
+                        }}
+                      >
+                        <Typography component="h5" variant="h5" color="text.primary">
+                          RD${tier.price}
+                        </Typography>
+                        <Typography variant="h6" color="text.secondary">
+                          /me
+                        </Typography>
+                      </Box>
+                      <PricingList>
+                        {tier.description.map((line) => (
+                          <Typography
+                            component="li"
+                            variant="subtitle1"
+                            align="center"
+                            key={line}
+                          >
+                            <CheckCircleOutlineIcon sx={{ color: "blue", fontSize: "1rem" }} /> {line}
+                          </Typography>
+                        ))}
+                      </PricingList>
+                    </CardContent>
+                    <Box sx={{ flexGrow: 1 }} />
+
+                    <CardActions sx={{ width: "13rem", alignSelf: 'center' }}>
+                      <Button
+                        fullWidth
+                        variant='contained'
+                        sx={{ background: tier.position === 'middle' ? "#168aad" : "#52b69a" }}
+                        type="button"
+                        onClick={() => console.log("Price clicked:", tier.price)}
+                      >
+                        {tier.buttonText}
+                      </Button>
+                    </CardActions>
+                    {Boolean(form.errors["pricing"] && form.touched["pricing"]) && <ErrorMessage name="pricing" component={FormHelperText}></ErrorMessage>}
+                  </Card>
+                </Box>
+
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
-    </Container>
+        </Container>
+      )}
+    </Field>
   );
 }
+
+
