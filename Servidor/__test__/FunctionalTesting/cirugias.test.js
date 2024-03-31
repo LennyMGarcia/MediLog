9881e20c678764d5949ea0607818b49b3b4a78ac
Lenny Garcia <Lennymgr27@gmail.com>
2024-03-31 12:40:42 -0400

    Feat: se agrego test de cirugias

--- Servidor/__test__/FunctionalTesting/cirugias.test.js
+++ Servidor/__test__/FunctionalTesting/cirugias.test.js
@@ -11,82 +11,81 @@ afterAll(() => {
     closeServer()
 });
 
-describe('TEST DE CASOS', () => {
+describe('TEST DE CIRUGIAS', () => {
 
     describe('Get', () => {
         let response;
 
         beforeAll(async () => {
-            response = await axios.get(`http://localhost:${PORT}/casos/9`);
+            response = await axios.get(`http://localhost:${PORT}/cirugias/1`);
         });
 
         it('Se espera un Ok o 200 como respuesta', async () => {
             expect(response.status).toBe(200);
         });
-        it('el ID debe ser 1', async () => {
-            expect(response.data.id).toBe(9);
+        it('el ID debe ser 9', async () => {
+            expect(response.data.id).toBe(1);
         });
-        it('Se espera que la descripcion sea Caso de Prueba', async () => {
-            expect(response.data.descripcion).toBe('Dolores Cronicos');
+        it('Se espera que la observaciones sea Se Detecto una anomalia causada por otras condiciones medicas', async () => {
+            expect(response.data.observaciones).toBe('Se Detecto una anomalia causada por otras condiciones medicas');
         });
-        it('Se espera que la descripcion sea paciente de prueba', async () => {
+        it('Se espera que el paciente sea Fulano Detal', async () => {
             expect(response.data.paciente).toBe('Fulano Detal');
         });
-        it('Se espera que el estado sea Activo', async () => {
-            expect(response.data.estado).toBe('Activo');
+        it('Se espera que el resultado sea Exito', async () => {
+            expect(response.data.resultado).toBe('Exito');
         });
         it('Se espera que el ID del paciente sea 1', async () => {
             expect(response.data.pacientes_id).toBe(1);
         });
-        it('Se espera que el ID del especialista sea 2', async () => {
-            expect(response.data.especialistas_id).toBe("1");
+        it('Se espera que el ID del especialista sea 60', async () => {
+            expect(response.data.especialistas_id).toBe(60);
         });
-        it('Se espera que la categoria sea Categoria de prueba', async () => {
-            expect(response.data.categoria).toBe('Dialisis');
+        it('Se espera que la categoria sea Estetica', async () => {
+            expect(response.data.categoria).toBe('Estetica');
         });
-        it('Se espera que la el seguimiento sea Seguimiento de prueba', async () => {
-            expect(response.data.seguimiento).toBe('Citas en 3 meses');
+        it('Se espera que la el motivo sea Cirugia estetica', async () => {
+            expect(response.data.motivo).toBe('Cirugia Estetica');
         });
         it('Debería retornar un error 400 cuando se hace una solicitud GET con un ID inválido', async () => {
             try {
-                response = await axios.get(`http://localhost:${PORT}/casos/abc`);
+                response = await axios.get(`http://localhost:${PORT}/cirugias/abc`);
 
                 expect(true).toBe(false);
             } catch (error) {
                 expect(error.isAxiosError).toBe(true);
                 expect(error.response.status).toBe(400);
-                expect(error.response.data.message).toBe("Numero de Identificacion Invalido. para campo de ' id '");
+                expect(error.response.data.message).toBe("Numero de Identificacion Invalido.");
             }
         });
     });
     describe('POST', () => {
         it('Debería agregar un nuevo caso', async () => {
             nock(`http://localhost:${PORT}`)
-                .post('/casos')
+                .post('/cirugias')
                 .reply(201, {
-                    id: 1,
-                    descripcion: 'Dolores cronicos',
+                    id: 2,
+                    motivo: 'Cirugia estetica',
                     paciente: 'Fulano Detal',
                     pacientes_id: 1,
-                    especialistas_id: "1",
-                    estado: 'Activo',
-                    categoria: 'Dialisis',
-                    seguimiento: 'Cita en 3 meses',
+                    especialistas_id: 60,
+                    resultado: 'Exito',
+                    categoria: 'Estetica',
                 });
-    
-            const response = await axios.post(`http://localhost:${PORT}/casos`, {
-                descripcion: 'Dolores cronicos',
+
+            const response = await axios.post(`http://localhost:${PORT}/cirugias`, {
+                id: 2,
+                motivo: 'Cirugia estetica',
                 paciente: 'Fulano Detal',
                 pacientes_id: 1,
-                especialistas_id: "1",
-                estado: 'Activo',
-                categoria: 'Dialisis',
-                seguimiento: 'Cita en 3 meses',
+                especialistas_id: 60,
+                resultado: 'Exito',
+                categoria: 'Estetica',
             });
-    
+
             expect(response.status).toBe(201);
-            expect(response.data.id).toBe(1);
-            expect(response.data.descripcion).toBe('Dolores cronicos');
+            expect(response.data.id).toBe(2);
+            expect(response.data.motivo).toBe('Cirugia estetica');
         });
     });
     describe("PUT", () => {
@@ -94,42 +93,40 @@ describe('TEST DE CASOS', () => {
             // Limpiar todos los interceptores de Nock despues  de cada prueba
             nock.cleanAll();
         });
-    
+
         it("Espera recibir una solicitud 200 con PUT", async () => {
             nock(`http://localhost:${PORT}`)
-                .put('/casos/9')
+                .put('/cirugias/1')
                 .reply(200);
-    
-            const response = await axios.put(`http://localhost:${PORT}/casos/9`, {
-                id:9,
-                descripcion: 'Caso de prueba',
-                paciente: 'Paciente de prueba',
+
+            const response = await axios.put(`http://localhost:${PORT}/cirugias/1`, {
+                id: 1,
+                motivo: 'Cirugia estetica',
+                paciente: 'Fulano Detal',
                 pacientes_id: 1,
-                especialistas_id: "1",
-                estado: 'Activo',
-                categoria: 'Categoria de prueba',
-                seguimiento: 'Seguimiento de prueba',
+                especialistas_id: 60,
+                resultado: 'Exito',
+                categoria: 'Estetica',
             });
 
             expect(response.status).toBe(200);
         });
-    
+
         it("Debería retornar un error 400 cuando se hace una solicitud PUT con un ID inválido", async () => {
 
             nock(`http://localhost:${PORT}`)
-                .put('/casos/abc')
+                .put('/cirugias/abc')
                 .reply(400, { message: "Numero de Identificacion Invalido. para campo de ' id '" });
-    
+
             try {
-                await axios.put(`http://localhost:${PORT}/casos/abc`, {
-                    id:"abc",
-                    descripcion: 'Caso de prueba',
-                    paciente: 'Paciente de prueba',
+                await axios.put(`http://localhost:${PORT}/cirugias/abc`, {
+                    id: "abc",
+                    motivo: 'Cirugia estetica',
+                    paciente: 'Fulano Detal',
                     pacientes_id: 1,
-                    especialistas_id: 2,
-                    estado: 'Activo',
-                    categoria: 'Categoria de prueba',
-                    seguimiento: 'Seguimiento de prueba',
+                    especialistas_id: 60,
+                    resultado: 'Exito',
+                    categoria: 'Estetica',
                 });
 
                 expect(true).toBe(false);
@@ -160,11 +157,11 @@ describe('TEST DE CASOS', () => {
         it("Debería retornar un error 400 cuando se hace una solicitud DELETE con un ID inválido", async () => {
 
             nock(`http://localhost:${PORT}`)
-                .delete('/casos/abc')
+                .delete('/cirugias/abc')
                 .reply(400, { message: "Numero de Identificacion Invalido. para campo de ' id '" });
     
             try {
-                await axios.delete(`http://localhost:${PORT}/casos/abc`);
+                await axios.delete(`http://localhost:${PORT}/cirugias/abc`);
                 
                 expect(true).toBe(false);
             } catch (error) {
@@ -173,6 +170,4 @@ describe('TEST DE CASOS', () => {
             }
         });
     });
-
-    
 });
